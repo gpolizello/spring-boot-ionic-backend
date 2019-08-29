ad79f42768263d3c769deb6c60ec92a6b7ec55e3
Gabriel Polizello <gpolizello@hotmail.com>
2019-08-29 09:46:56 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/polizello/server/services/validation/ClientInsertValidator.java
+++ src/main/java/com/polizello/server/services/validation/ClientInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.polizello.server.domain.Cliente;
 import com.polizello.server.domain.enums.TipoCliente;
 import com.polizello.server.dto.ClienteNewDTO;
+import com.polizello.server.repositories.ClienteRepository;
 import com.polizello.server.resources.exceptions.FieldMessage;
 import com.polizello.server.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClientInsertValidator implements ConstraintValidator<ClienteInsert,
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		for (FieldMessage e : list) {
