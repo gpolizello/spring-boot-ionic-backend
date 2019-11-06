41152187d712de8aae2da8cabef32d83012b3f7c
Gabriel Polizello <gpolizello@hotmail.com>
2019-11-06 14:33:01 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/polizello/server/services/DBService.java
+++ src/main/java/com/polizello/server/services/DBService.java
@@ -20,6 +20,7 @@ import com.polizello.server.domain.PagamentoComCartao;
 import com.polizello.server.domain.Pedido;
 import com.polizello.server.domain.Produto;
 import com.polizello.server.domain.enums.EstadoPagamento;
+import com.polizello.server.domain.enums.Perfil;
 import com.polizello.server.domain.enums.TipoCliente;
 import com.polizello.server.repositories.CategoriaRepository;
 import com.polizello.server.repositories.CidadeRepository;
@@ -120,13 +121,19 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria Silva", "gpolizello@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA, senha);
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "ga_jogos@hotmail.com", "51193224098", TipoCliente.PESSOAFISICA, senha);
+		cli2.addPerfil(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
-		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "3877012", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "3877012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "21006", null, "Centro", "357814", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 		
 		/******** CRIANDO OS PEDIDOS E OS PAGAMENTOS***********/
